ed444027ecd3c54416e285aa9ea3e7305f76336f
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2020-10-20 00:02:10 +0200

    Fix run examples script 2
    There was a bug that exported all backup apkgs to the file '..apkg' which is now fixed

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -22,7 +22,7 @@ done
 find . -maxdepth 1 -type d -name "backup_*" | while read backup_dir
 do
 	../run.sh "examples/$backup_dir/document.md" \
-	          -o-anki "examples/$(dirname "$backup_dir").apkg" \
+	          -o-anki "examples/$(basename "$backup_dir").apkg" \
 	          -file-dir "examples/$backup_dir" \
 	          "$@"
 done
