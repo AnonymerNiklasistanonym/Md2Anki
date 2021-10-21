b6b71902133112938c9aa4a09b57d9fd0199c88b
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2021-10-21 13:05:48 +0200

    Update setup.py and add md2anki(.py) to the python bin directory
    TODO: The additional necessary files (js,css) are missing to actually get it to work

---
+++ scripts/md2anki
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import sys
+
+import md2anki
+
+if __name__ == "__main__":
+    cliArgs = md2anki.parse_cli_args(sys.argv[1:])
+    exitCode = md2anki.main(cliArgs)
+    exit(exitCode)
--- setup.py
+++ setup.py
@@ -14,7 +14,23 @@ setup(
     project_urls={
         "Bug Tracker": "https://github.com/AnonymerNiklasistanonym/Md2Anki/issues",
     },
-    packages=find_packages(include=["md2anki.py"]),
+    packages=find_packages(),
+    py_modules=["md2anki"],
+    package_dir={"": "./"},
+    package_data={
+        "md2anki": [
+            "highlightJs_renderer.js",
+            "kaTex_renderer.js",
+            "stylesheet.css",
+        ],
+    },
+    include_package_data=True,
+    license='MIT',
+    scripts=["scripts/md2anki.py", "scripts/md2anki"],
+    install_requires=[
+        "genanki>=0.11.0",
+        "Markdown>=3.3.4",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
